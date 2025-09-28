f849f3ee31e94143765d698e6645e5f26c720337
flipityflop <shemzo780@gmail.com>
2025-09-28 11:33:38 -0400

    learn: match

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,13 +1,28 @@
-enum IpAddr {
-    V4(u8, u8, u8, u8),
-    V6(String),
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
 }
 
-fn main() {
-    let home = IpAddr::V4(127, 0, 0, 1);
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
 
-    let loopback = IpAddr::V6(String::from("::1"));
+fn main () {
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+}
 
-    println!("{loopback}");
-    println!("{home}");
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        },
+    }
 }
---
+++ enums/target/debug/enums.d
@@ -0,0 +1,1 @@
+C:\Users\Shlomo\ Friedman\documents\code\rust-learning\enums\target\debug\enums.exe: C:\Users\Shlomo\ Friedman\documents\code\rust-learning\enums\src\main.rs
