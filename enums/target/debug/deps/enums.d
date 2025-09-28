e6bb98a1ff510d4496369d095777ef61b31c34c4
flipityflop <shemzo780@gmail.com>
2025-09-28 11:18:07 -0400

    learn: enums

---
+++ enums/target/debug/deps/enums.d
@@ -0,0 +1,5 @@
+C:\Users\Shlomo Friedman\documents\code\rust-learning\enums\target\debug\deps\enums.d: src\main.rs
+
+C:\Users\Shlomo Friedman\documents\code\rust-learning\enums\target\debug\deps\enums.exe: src\main.rs
+
+src\main.rs:
