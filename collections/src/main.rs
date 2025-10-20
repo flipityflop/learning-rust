f3d870c06dbfc35314e8769d3c9d47955ae2f896
flipityflop <shemzo780@gmail.com>
2025-10-20 08:51:21 -0400

    learn: string iteration

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,12 +1,19 @@
 fn main() {
-    let v = vec![1, 2, 3, 4, 5];
+    let s1 = String::from("string 1");
+    let s2 = String::from("string 2");
+    let s3 = s1 + &s2;
 
-    let third: &i32 = &v[2];
-    println!("the third element is {third}");
+    println!("s3 is {s3}");
 
-    let third: Option<&i32> = v.get(2);
-    match third {
-        Some(third) => println!("The third element is {third}"),
-        None => println!("There is no third element"),
+    let s4 = String::from("tic");
+    let s5 = String::from("tac");
+    let s6 = String::from("toe");
+
+    let s7 = format!("{s4} {s5} {s6}");
+
+    println!("{s7}");
+
+    for c in s7.chars() {
+        println!("{c}");
     }
 }
